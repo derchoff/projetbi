f7e9df574e98cbad103dbc568fdc20553c0df0f2
derchoff@gmail.com <derchoff@gmail.com>
2019-05-04 23:42:43 +0200

    deploy script v2

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -7,11 +7,14 @@
 startme() {
 
     #pwgen n'est pas installer sur la VPS  et je ne souhaite pas gérer une installation via le script
-    DB_ROOT_PWD=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`;
+    DB_ROOT_PWD=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`;    
+    DB_TOMCAT_PWD=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`;    
+    BI_SRC_PATH="/usr/src/projetbi/BDD";
 
     echo '---------------------------------------';
     echo '| GENERATED PASSWORD';
-    echo "| Root            : $DB_ROOT_PWD";    
+    echo "| Db Root            : $DB_ROOT_PWD";    
+    echo "| Tomcat Root        : $DB_TOMCAT_PWD";    
     echo '---------------------------------------';
 
     export DB_ROOT_PWD;
@@ -24,25 +27,27 @@ startme() {
         echo "Waiting for database connection..."
         # wait for 5 seconds before check again
         sleep 5
-    done;
-    
-    echo $PWD;
+    done;   
     
     echo 'execute le script SQL pour installer les tables de bdd'
-    mysql db_name < ../BDD/MCD_2019_04_28.sql
-    
-    $ mysql -h  mysqldb -u "root" "-p${DB_ROOT_PWD}" "db_darties" < "../BDD/MCD_2019_04_28.sql";   
+#    docker exec mysqldb /bin/bash -c 'chown -R mysql:mysql /var/lib/mysql'
+#   docker exec mysqldb /bin/bash -c "mysql -h mysqldb -u root -p$DB_ROOT_PWD db_darties < $BI_SRC_PATH/MCD_2019_04_28.sql";
 }
 
 stopme() {
     docker-compose down;
-    docker system prune --force;
+    docker system prune --force --volumes;
+}
+
+logmysqldb() {
+     docker logs -f mysqldb;
 }
 
 case "$1" in 
     start)   startme ;;
     stop)    stopme ;;
     restart) stopme; startme ;;
+    log) logmysqldb ;;
     *) echo "usage: $0 start|stop|restart" >&2
        exit 1
        ;;
