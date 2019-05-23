93b7e52050abad68721d837ec6981be528820a12
Denis Erchoff <Denis.Erchoff@fmc-ag.com>
2019-05-23 18:19:53 +0200

    intégration des magasins adaptées au nouveau MCD

--- TALEND/poms/jobs/process/projetBI/gestion_dossier_reussi_0.1/src/main/templates/Job_run_template.ps1
+++ TALEND/poms/jobs/process/projetBI/gestion_dossier_reussi_0.1/src/main/templates/Job_run_template.ps1
@@ -1,3 +1,3 @@
 $fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/log4j-1.2.17.jar;../lib/filecopy.jar;../lib/dom4j-1.6.1.jar;../lib/talendcsv.jar;../lib/talend_file_enhanced_20070724.jar;gestion_dossier_reussi_0_1.jar;' projetbi.gestion_dossier_reussi_0_1.gestion_dossier_reussi  %*
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/log4j-1.2.17.jar;../lib/filecopy.jar;../lib/dom4j-1.6.1.jar;../lib/talendcsv.jar;../lib/talend_file_enhanced_20070724.jar;gestion_dossier_reussi_0_1.jar;' projet_bi_2.gestion_dossier_reussi_0_1.gestion_dossier_reussi  %*
