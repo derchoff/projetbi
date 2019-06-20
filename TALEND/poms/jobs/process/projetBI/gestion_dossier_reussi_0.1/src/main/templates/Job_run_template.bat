e3eb4f960ce0e4ada4b5067d587517eb98cf325c
derchoff@gmail.com <derchoff@gmail.com>
2019-06-21 01:58:06 +0200

    Fix intégration objectif. Fichier excel de test

--- TALEND/poms/jobs/process/projetBI/gestion_dossier_reussi_0.1/src/main/templates/Job_run_template.bat
+++ TALEND/poms/jobs/process/projetBI/gestion_dossier_reussi_0.1/src/main/templates/Job_run_template.bat
@@ -1,3 +1,3 @@
 %~d0
 cd %~dp0
-java -Dtalend.component.manager.m2.repository=%cd%/../lib -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/activation.jar;../lib/log4j-1.2.17.jar;../lib/mail-1.4.jar;../lib/mysql-connector-java-8.0.12.jar;../lib/filecopy.jar;../lib/dom4j-1.6.1.jar;../lib/talendcsv.jar;../lib/talend_file_enhanced_20070724.jar;gestion_dossier_reussi_0_1.jar; projet_bi_4.gestion_dossier_reussi_0_1.gestion_dossier_reussi  %*
+java -Dtalend.component.manager.m2.repository=%cd%/../lib -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/activation.jar;../lib/log4j-1.2.17.jar;../lib/mail-1.4.jar;../lib/mysql-connector-java-8.0.12.jar;../lib/filecopy.jar;../lib/dom4j-1.6.1.jar;../lib/talendcsv.jar;../lib/talend_file_enhanced_20070724.jar;gestion_dossier_reussi_0_1.jar; projet_bi_5.gestion_dossier_reussi_0_1.gestion_dossier_reussi  %*
